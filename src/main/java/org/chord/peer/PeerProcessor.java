d804042029a212e08574b6e89a35b5c2948a04fb
Caleb Carlson <caleb.carlson@hpe.com>
2021-10-15 11:17:16 -0600

    Update finger table upon successor/pred notif

--- src/main/java/org/chord/peer/PeerProcessor.java
+++ src/main/java/org/chord/peer/PeerProcessor.java
@@ -158,6 +158,7 @@ public class PeerProcessor extends Processor {
     public void processPredecessorNotification(PredecessorNotification message) throws IOException {
         this.peer.setPredecessor(message.getPeerId());
         log.info("Updated predecessor to peer: {}", message.getPeerId());
+        this.peer.updateFingerTable(message.getPeerId());
         sendResponse(this.socket, new StatusMessage(
                 Host.getHostname(),
                 Host.getIpAddress(),
@@ -168,6 +169,7 @@ public class PeerProcessor extends Processor {
     public void processSuccessorNotification(SuccessorNotification message) throws IOException {
         this.peer.setSuccessor(message.getPeerId());
         log.info("Updated successor to peer: {}", message.getPeerId());
+        this.peer.updateFingerTable(message.getPeerId());
         sendResponse(this.socket, new StatusMessage(
                 Host.getHostname(),
                 Host.getIpAddress(),
